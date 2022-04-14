972112128abc8f28a09b2ccf7ffce8b721684b5d
mathias <mathiasgheno@gmail.com>
2022-04-13 22:25:00 -0300

    chore: initial config for logs with loglevel

--- src/api/features/features.entity.mjs
+++ src/api/features/features.entity.mjs
@@ -1,8 +1,16 @@
 import { DynamoDBEntity } from '../../common/DynamoDBEntity';
+import log from 'loglevel';
 
 export const featuresEntityFactory = async function () {
+  const ENTITY = 'features';
+
+  log.info(`Creating table of entity ${ENTITY}.`);
+
   const entity = new DynamoDBEntity('features');
   await entity.createTable();
+
+  log.info(`Everyhing ok with entity ${ENTITY}.`);
+
   return {
     getIntance: function() {
       return entity;
--- src/api/index.mjs
+++ src/api/index.mjs
@@ -1,9 +1,13 @@
+import log from 'loglevel';
 import { featuresEntityFactory } from './features/features.entity.mjs';
 import { usersEntityFactory } from './users/users.entity.mjs';
 
 async function main() {
+  log.setLevel(process.env.LOG_LEVEL || 'info');
+
   const features = await featuresEntityFactory();
   const users = await usersEntityFactory();
+
   return {
     features: features.getIntance(),
     users: users.getIntance(),
