fd2edd9997aec8148edfbace44e2c8d6dfb6becd
mathias <mathiasgheno@gmail.com>
2022-05-17 22:25:47 -0300

    feat: roles and systems in toggle input

--- packages/ui/src/routes/Toggle/Service/Toggle.service.js
+++ packages/ui/src/routes/Toggle/Service/Toggle.service.js
@@ -1,4 +1,3 @@
-
 export function createToggle(body, signal) {
   const url = 'https://l3ut0axl99.execute-api.sa-east-1.amazonaws.com/dev/features';
   return fetch(url, {
@@ -7,4 +6,16 @@ export function createToggle(body, signal) {
     body: JSON.stringify(body)
   })
     .then((res) => res.json())
+}
+
+export function listSystems(signal) {
+  const url = 'https://l3ut0axl99.execute-api.sa-east-1.amazonaws.com/dev/systems';
+  return fetch(url, { method: 'GET', signal })
+    .then((res) => res.json())
+}
+
+export function listRoles(signal) {
+  const url = 'https://l3ut0axl99.execute-api.sa-east-1.amazonaws.com/dev/roles';
+  return fetch(url, { method: 'GET', signal })
+    .then(response => response.json());
 }
--- packages/ui/src/routes/Toggle/Toggle.jsx
+++ packages/ui/src/routes/Toggle/Toggle.jsx
@@ -1,22 +1,96 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container } from './Toggle.styled';
-import { createToggle } from './Service/Toggle.service';
+import { createToggle, listRoles, listSystems } from './Service/Toggle.service';
 
 export const Toggle = () => {
   const [ name, setName ] = useState(null);
+  const [ allSystems, setAllSystems ] = useState([]);
+  const [ allRoles, setAllRoles ] = useState([]);
   const [ loading, setLoading ] = useState(false);
+  const [ submitting, setSubmitiing ] = useState(false);
+  const [ controller ] = useState(new AbortController());
+  const [ error, setError ] = useState(false);
 
   const handleOnSubmit = (event) => {
-    setLoading(true);
+    setSubmitiing(true);
     event.preventDefault();
+    const justChecked = ({ check }) => check;
+    const justId = ({ id }) => id;
+    const generateList = (list) => list.filter(justChecked).map(justId)
     const dto = {
       name,
       description: 'Testing Post',
-      allowedRoles: [],
-      systems: [],
+      allowedRoles: generateList(allRoles),
+      systems: generateList(allSystems),
     }
     createToggle(dto)
-      .finally(() => setLoading(false))
+      .finally(() => setSubmitiing(false))
+  }
+
+  const _list = useCallback(() => {
+    setLoading(true);
+    setError(false);
+
+    const handleSystemCallback = (systems) => {
+      const systemsCheckBox = systems.map((system) => ({ ...system, check: false }));
+      setAllSystems(systemsCheckBox);
+    }
+
+    const handleRolesCallback = (roles) => {
+      const rolesCheckBox = roles.map((role) => ({ ...role, check: false }));
+      setAllRoles(rolesCheckBox);
+    }
+
+    const { signal } = controller;
+    return Promise.all([
+      listSystems(signal).then(handleSystemCallback),
+      listRoles(signal).then(handleRolesCallback),
+    ]).finally(() => setLoading(false))
+  }, []);
+
+  useEffect(() => {
+    _list();
+    return () => controller.abort();
+  }, []);
+
+  const handleOnClickRoles = (id) => {
+    const rolesCheckBox = allRoles.map((role) => ({
+      ...role,
+      check: role.id === id
+        ? true
+        : role.id,
+    }));
+    setAllRoles(rolesCheckBox);
+  }
+
+  const handleOnClickSystems = (id) => {
+    const systemsCheckBox = allSystems.map((system) => ({
+      ...system,
+      check: system.id === id
+        ? true
+        : system.id,
+    }));
+    setAllSystems(systemsCheckBox);
+  }
+
+  if(error) {
+    return (
+      <Container>
+        <a href="#/toggles">Voltar</a>
+        <h3>New Toggle</h3>
+        <span>Oops! There is a Bug here.</span>
+      </Container>
+    )
+  }
+
+  if(loading) {
+    return (
+      <Container>
+        <a href="#/toggles">Voltar</a>
+        <h3>New Toggle</h3>
+        <span>Loading...</span>
+      </Container>
+    )
   }
 
   return (
@@ -29,11 +103,46 @@ export const Toggle = () => {
           type="text"
           id="toggle"
           value={name}
+          disabled={submitting}
           onChange={(event) => {
             setName(event.target.value);
           }}
         />
-        <button type="submit" disabled={loading}>{loading ? '...loading': 'Add'}</button>
+        <label htmlFor="systems">Systems</label>
+        {
+          allSystems.map((system) => (
+            <div key={system.id}>
+              <input
+                type="checkbox"
+                id={system.name}
+                name="systems"
+                value={system.id}
+                disabled={submitting}
+                onClick={() => handleOnClickSystems(system.id)}
+              />
+              <label htmlFor={system.name}>{system.name}</label>
+            </div>
+          ))
+        }
+        <label htmlFor="roles">Roles</label>
+        {
+          allRoles.map((role) => (
+            <div key={role.id}>
+              <input
+                type="checkbox"
+                id={role.name}
+                name="roles"
+                value={role.id}
+                disabled={submitting}
+                onClick={() => handleOnClickRoles(role.id)}
+              />
+              <label htmlFor={role.name}>{role.name}</label>
+            </div>
+          ))
+        }
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" cols="30" rows="10" />
+        <button type="submit" disabled={submitting}>{loading ? '...loading': 'Add'}</button>
       </form>
     </Container>
   );
